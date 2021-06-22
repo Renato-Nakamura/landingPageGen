a7cc1f5cbf38b7310979ecd9d02aca0583497845
Renato-Nakamura <renato.yukio30@hotmail.com>
2021-06-22 11:32:00 -0300

    completo sem cores e fontes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: false
-})
+let nome = document.querySelector("#nome")
+let email = document.querySelector("#email")
+let msg = document.querySelector("#msg")
+
+function validar(){
+  if(nome.value.length>2){
+    if(email.value.indexOf('@')==-1){
+      alert("Insira um email valido")
+    }else{
+      if(msg.value.length>0){
+        alert("Mensagem enviada com sucesso para a equipe! Agradecemos o apoio")
+      }else{
+        alert("A mensagem deve possuir conteúdo")
+      }
+    }
+  }else{
+    alert("O nome precisa conter pelo menos 3 caracteres")
+  }
+}
